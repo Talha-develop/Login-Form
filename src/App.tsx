3255f532b890fdd4460e65cd7764bb3eedca4eb3
Talha-develop <talhaasad5584@gmail.com>
2025-09-24 22:30:26 +0500

    finally used form in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginForm from "./components/LoginForm";
 
-function App() {
-  return <div className="text-amber-300">App</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300">
+      <LoginForm />
+    </div>
+  );
+};
 
 export default App;
